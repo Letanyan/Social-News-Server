958f3355481ed42aa3796725c918ae42ed8f480c
Letanyan <letanyan.a@gmail.com>
2022-10-01 00:47:17 +0200

    added error messages and use os port if available

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.GET("/", index)
 	router.GET("/albums", getAlbums)
 
-	router.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := router.Run(":" + port); err != nil {
+		log.Panicf("error: %s", err.Error())
+	}
+}
+
+func index(c *gin.Context) {
+	c.String(http.StatusOK, "Index")
 }
 
 // getAlbums responds with the list of all albums as JSON.
