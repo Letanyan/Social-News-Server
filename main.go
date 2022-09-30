9dc3691e8b22aeeeb74f39b51b04b38e05a999c8
Letanyan <letanyan.a@gmail.com>
2022-10-01 00:43:59 +0200

    remove localhost

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-    router.GET("/albums", getAlbums)
+	router.GET("/albums", getAlbums)
 
-    router.Run("localhost:8080")
+	router.Run(":8080")
 }
 
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, "[[Albums]]")
+	c.IndentedJSON(http.StatusOK, "[[Albums]]")
 }
