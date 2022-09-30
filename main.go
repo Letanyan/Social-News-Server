fef2f370ada575f81ee0b82f339362362fbe36d1
Letanyan <letanyan.a@gmail.com>
2022-09-30 23:48:53 +0200

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+    router.GET("/albums", getAlbums)
+
+    router.Run("localhost:8080")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, "[[Albums]]")
+}
