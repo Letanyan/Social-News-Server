52dd0b2a101ead94958790f3756d479b84d39655
Letanyan <letanyan.a@gmail.com>
2023-03-12 05:20:37 +0200

    optimise for you query

--- db.go
+++ db.go
@@ -771,7 +771,13 @@ func DBFunctionSetup(db *sql.DB) {
 	RETURNS DOUBLE PRECISION LANGUAGE plpgsql STRICT AS $$
 	BEGIN
 		RETURN accumulator + COALESCE(x / NULLIF(x + y, 0), 0.0);
-	END; $$; 
+	END; $$;
+
+	CREATE OR REPLACE FUNCTION sumRatioCombine (cagg DOUBLE PRECISION, dagg DOUBLE PRECISION)
+	RETURNS DOUBLE PRECISION LANGUAGE plpgsql STRICT AS $$
+	BEGIN
+		RETURN cagg + dagg;
+	END; $$;
 
 	CREATE OR REPLACE FUNCTION sumRatioFinal (accumulator DOUBLE PRECISION)
 	RETURNS DOUBLE PRECISION LANGUAGE plpgsql STRICT AS $$
@@ -783,7 +789,9 @@ func DBFunctionSetup(db *sql.DB) {
 	CREATE OR REPLACE AGGREGATE sumRatio (x DOUBLE PRECISION, y DOUBLE PRECISION) (
 		sfunc = sumRatioAgg,
 		stype = DOUBLE PRECISION,
+		combinefunc = sumRatioCombine,
 		finalfunc = sumRatioFinal,
+		parallel = safe,
 		initcond = 0
 	);`
 	_, e = db.Exec(createAggRatioValue)
@@ -811,6 +819,64 @@ func DBFunctionSetup(db *sql.DB) {
 	);`
 	_, e = db.Exec(createAggWeightRatio)
 	DidFail(e, "create aggregate weight function")
+}
+
+func DBCreateCAggregateFunctions(db *sql.DB) {
+	createCSumRatio := `
+	CREATE OR REPLACE FUNCTION c_sumRatioAgg(DOUBLE PRECISION, DOUBLE PRECISION, DOUBLE PRECISION) 
+	RETURNS DOUBLE PRECISION 
+	AS '/home/letan/psqlc/func.so', 'c_sumRatioAgg'
+	LANGUAGE C STRICT;
+
+	CREATE OR REPLACE FUNCTION c_sumRatioCombine(DOUBLE PRECISION, DOUBLE PRECISION) 
+	RETURNS DOUBLE PRECISION 
+	AS '/home/letan/psqlc/func.so', 'c_sumRatioCombine'
+	LANGUAGE C STRICT;
+
+	CREATE OR REPLACE FUNCTION c_sumRatioFinal(DOUBLE PRECISION) 
+	RETURNS DOUBLE PRECISION 
+	AS '/home/letan/psqlc/func.so', 'c_sumRatioFinal'
+	LANGUAGE C STRICT;
+
+	CREATE OR REPLACE AGGREGATE c_sumRatio(DOUBLE PRECISION, DOUBLE PRECISION) (
+		sfunc = c_sumRatioAgg,
+		stype = DOUBLE PRECISION,
+		combinefunc = c_sumRatioCombine,
+		finalfunc = c_sumRatioFinal,
+		parallel = safe,
+		initcond = 0
+	);
+	`
+	_, e := db.Exec(createCSumRatio)
+	DidFail(e, "create C sum ratio function")
+
+	createCScoreValue := `
+	CREATE OR REPLACE FUNCTION c_scoreValueFactorAgg(ScoreValueType, DOUBLE PRECISION, DOUBLE PRECISION, DOUBLE PRECISION) 
+	RETURNS ScoreValueType
+	AS '/home/letan/psqlc/func.so', 'c_scoreValueFactorAgg'
+	LANGUAGE C STRICT;
+
+	CREATE OR REPLACE FUNCTION c_scoreValueFactorCombine(ScoreValueType, ScoreValueType) 
+	RETURNS ScoreValueType
+	AS '/home/letan/psqlc/func.so', 'c_scoreValueFactorCombine'
+	LANGUAGE C STRICT;
+
+	CREATE OR REPLACE FUNCTION c_scoreValueFactorFinal(ScoreValueType) 
+	RETURNS DOUBLE PRECISION 
+	AS '/home/letan/psqlc/func.so', 'c_scoreValueFactorFinal'
+	LANGUAGE C STRICT;
+
+	CREATE OR REPLACE AGGREGATE c_scoreValueFactor (DOUBLE PRECISION, DOUBLE PRECISION, DOUBLE PRECISION) (
+		sfunc = c_scoreValueFactorAgg,
+		stype = ScoreValueType,
+		combinefunc = c_ScoreValueFactorCombine,
+		finalfunc = c_scoreValueFactorFinal,
+		parallel = safe, 
+		initcond = '(0, 0, 0, 0)'
+	);
+	`
+	_, e = db.Exec(createCScoreValue)
+	DidFail(e, "create C score value function")
 }
 
 func DBDeleteTable(db *sql.DB, name string) {
--- dbposts.go
+++ dbposts.go
@@ -394,18 +394,27 @@ func DBGetPosts(db *sql.DB, userId int64, tags []int64, origin []string, popular
 	if len(popularIn) > 0 {
 		locArray = DBGetLocationIndex(db, popularIn)
 	}
-	getPosts := SQLGetItems(db, "Posts p", voteTable, SQLFieldsForPostResultAlias(),
-		SQLFieldsForPostResult(), joins, locArray, cond, usingVotesTable,
-		upvotes, downvotes,
-		sortOrder, limit, offset, startDate, endDate, forUser, search)
 
-	rows, e := db.Query(getPosts)
 	result := []PostResult{}
-	if DidFail(e, "get posts\n", getPosts) {
-		return result
+	// increase total posts to consider by months as more items requested
+	for i := 1; i <= 12; i += 1 {
+		cond = append(cond, fmt.Sprintf("p.createdAt > ((now() at time zone 'utc') - interval '%d month')", i))
+		getPosts := SQLGetItems(db, "Posts p", voteTable, SQLFieldsForPostResultAlias(),
+			SQLFieldsForPostResult(), joins, locArray, cond, usingVotesTable,
+			upvotes, downvotes,
+			sortOrder, limit, offset, startDate, endDate, forUser, search)
+
+		rows, e := db.Query(getPosts)
+		if DidFail(e, "get posts\n", getPosts) {
+			return result
+		}
+
+		result = ScanPostResults(rows, usingVotesTable, len(search) > 0 && sortOrder == soRank)
+		if len(result) != 0 {
+			break
+		}
 	}
 
-	result = ScanPostResults(rows, usingVotesTable, len(search) > 0 && sortOrder == soRank)
 	if forUser > 0 && len(result) == 0 { // if no more recommended show 2nd degree recommended
 		result = DBGetSimilarPosts(db, forUser, 0, sortOrder, limit, offset)
 		if len(result) == 0 { // show trending if no recommended
